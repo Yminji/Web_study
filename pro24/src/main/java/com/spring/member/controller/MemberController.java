4976157d5881a7066cab0c25b8663cd9033832db
Yminji <ymg0818@naver.com>
2022-12-22 18:34:28 +0900

    스프링과 마이바티스 연동하기

---
+++ pro24/src/main/java/com/spring/member/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.spring.member.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+interface MemberController {
+
+	ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+	ModelAndView addMember(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	
+	ModelAndView removeMember(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+}
